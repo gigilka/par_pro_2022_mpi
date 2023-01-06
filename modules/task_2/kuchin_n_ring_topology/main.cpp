fbd02e232eeb860d01b5bfeb13ec00d2ca95e20c
Nick <kuchin-02@mail.ru>
2023-01-06 04:13:58 +0300

    4

--- modules/task_2/kuchin_n_ring_topology/main.cpp
+++ modules/task_2/kuchin_n_ring_topology/main.cpp
@@ -80,16 +80,19 @@ TEST(MPI_2, MPI_2_ring_5_Test) {
 }
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    MPI_Init(&argc, &argv);
+    int main(int argc, char** argv) {
+        ::testing::InitGoogleTest(&argc, argv);
+        MPI_Init(&argc, &argv);
 
-    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-    ::testing::TestEventListeners& listeners =
-        ::testing::UnitTest::GetInstance()->listeners();
+        ::testing::AddGlobalTestEnvironment(
+            new GTestMPIListener::MPIEnvironment);
+        ::testing::TestEventListeners& listeners =
+            ::testing::UnitTest::GetInstance()->listeners();
 
-    listeners.Release(listeners.default_result_printer());
-    listeners.Release(listeners.default_xml_generator());
+        listeners.Release(listeners.default_result_printer());
+        listeners.Release(listeners.default_xml_generator());
 
-    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-    return RUN_ALL_TESTS();
+        listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+        return RUN_ALL_TESTS();
+    }
 }
